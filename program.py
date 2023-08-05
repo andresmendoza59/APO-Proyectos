ce9a6b98cefcaa0d788151b3089ce701042a4be2
Andrés Mendoza <amendoza453@soyudemedellin.edu.co>
2023-08-05 10:02:18 -0500

    Se añade resta en 'rama-dos'

--- program.py
+++ program.py
@@ -1,6 +1,6 @@
-# Programa para sumar números
+# Programa para restar números
 
 n1 = int(input("Escriba el primer número: "))
 n2 = int(input("Escriba el segundo número: "))
 
-print(f"La suma es {n1 + n2}")
+print(f"La resta es {n1 - n2}")
