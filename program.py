0dc6c8766eb7a13ac0de973cc86b36772f338331
Andrés Mendoza <amendoza453@soyudemedellin.edu.co>
2023-08-05 11:14:49 -0500

    Se añaden más comentarios

--- program.py
+++ program.py
@@ -2,6 +2,7 @@
 # Programa para dividir números
 # Programa para multiplicar números
 # Programa para restar números
+# Programa para sumar números
 
 n1 = int(input("Escriba el primer número: "))
 n2 = int(input("Escriba el segundo número: "))
