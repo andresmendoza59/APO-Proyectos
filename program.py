8b94f20496c1acd4305342ad5460b817600307e6
Andrés Mendoza <amendoza453@soyudemedellin.edu.co>
2023-08-05 10:43:31 -0500

    Se añade rama con funcionalidad de calcular potencias

--- program.py
+++ program.py
@@ -1,6 +1,6 @@
-# Programa para dividir números
+# Programa para calcular potencias de números
 
-n1 = int(input("Escriba el primer número: "))
-n2 = int(input("Escriba el segundo número: "))
+n1 = int(input("Escriba la base de la potencia: "))
+n2 = int(input("Escriba el exponente de la potencia: "))
 
-print(f"La división total es: {n1 / n2}")
+print(f"El resultado es: {n1 ** n2}")
